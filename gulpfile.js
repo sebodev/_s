c1893ea4f20ae98c1593955cc8518e482020d4b7
Rob Skidmore <rob@robskidmore.me>
2016-01-27 22:41:03 -0700

    Adding gulp tasks

---
+++ gulpfile.js
@@ -0,0 +1,77 @@
+var gulp = require('gulp');
+var browserSync = require('browser-sync');
+var plugins = require( 'gulp-load-plugins' )({ camelize: true });
+
+var url = 'gulp.dev'
+
+var dir = {
+    sass: './sass/',
+    img: './img/',
+    js: './js/',
+    bower: './bower_components'
+}
+
+gulp.task('styles', function() {
+    return plugins.rubySass(dir.sass + 'style.scss', {
+        style: 'expanded',
+        loadPath: dir.bower
+    })
+    .pipe(plugins.plumber())
+    .pipe(plugins.autoprefixer('last 2 versions', 'ie 9', 'ios 6', 'android 4'))
+    .pipe(plugins.combineMediaQueries())
+    .pipe(plugins.csscomb())
+    .pipe(plugins.plumber.stop())
+    .pipe(gulp.dest('./'))
+    .pipe(browserSync.reload({ stream: true }));
+});
+
+gulp.task('images', function() {
+    return gulp.src(dir.img + 'src/*(*.png|*.jpg|*.jpeg|*.gif)')
+    .pipe(plugins.imagemin())
+    .pipe(gulp.dest(dir.img));
+});
+
+gulp.task('scripts', ['scripts-lint', 'scripts-main', 'scripts-admin']);
+
+gulp.task('scripts-lint', function() {
+    return gulp.src(dir.js + '**/*.js')
+    .pipe(plugins.eslint())
+    .pipe(plugins.eslint.format());
+});
+
+gulp.task('scripts-admin', function() {
+    return gulp.src([
+        dir.js + 'customizer.js'
+    ])
+    .pipe(plugins.concat('admin.js'))
+    .pipe(gulp.dest(dir.js))
+    .pipe(plugins.rename({suffix: '.min'}))
+    .pipe(plugins.uglify())
+    .pipe(gulp.dest(dir.js));
+});
+
+gulp.task('scripts-main', function() {
+    return gulp.src([
+        dir.js + 'navigation.js',
+        dir.js + 'skip-link-focus-fix.js'
+    ])
+    .pipe(plugins.concat('main.js'))
+    .pipe(gulp.dest(dir.js))
+    .pipe(plugins.rename({suffix: '.min'}))
+    .pipe(plugins.uglify())
+    .pipe(gulp.dest(dir.js));
+});
+
+gulp.task('browser-sync', ['watch'], function() {
+    browserSync.init({
+        proxy: url
+    });
+});
+
+gulp.task('watch', function(){
+    gulp.watch(dir.sass+'**/*.scss', ['styles']);
+    gulp.watch(dir.img+'src/**/*(*.png|*.jpg|*.jpeg|*.gif)', ['images']);
+    gulp.watch(dir.js+'**/*.js', ['scripts']);
+});
+
+gulp.task('default', ['browser-sync']);
